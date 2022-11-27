2849608e79dd074748b87b23fbafef2a0c9b9f8c
iness <ines.lachkhem@esprit.tn>
2022-11-27 11:39:31 +0100

    ajout d'un fichier Glade_ines

---
+++ Glade_ines/src/callbacks.c
@@ -0,0 +1,59 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_ILradiobuttonhomme_group_changed    (GtkRadioButton  *radiobutton,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_ILradiobuttonfemme_group_changed    (GtkRadioButton  *radiobutton,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_ILButtonajoutera_clicked            (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+gboolean
+on_comboboxrolea_popdown               (GtkComboBox     *combobox,
+                                        gpointer         user_data)
+{
+
+  return FALSE;
+}
+
+
+void
+on_ILbuttonmodifiera_clicked           (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_ILbuttonsupprimera_clicked          (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
