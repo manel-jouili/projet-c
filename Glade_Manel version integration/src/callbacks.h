767273beecf183d8db1c3345b89df596c577128a
manel-jouili <115433254+manel-jouili@users.noreply.github.com>
2022-12-09 15:07:57 +0100

    Add files via upload

---
+++ Glade_Manel version integration/src/callbacks.h
@@ -0,0 +1,198 @@
+#include <gtk/gtk.h>
+
+
+void
+on_ILradiobuttonhomme_group_changed    (GtkRadioButton  *radiobutton,
+                                        gpointer         user_data);
+
+void
+on_ILradiobuttonfemme_group_changed    (GtkRadioButton  *radiobutton,
+                                        gpointer         user_data);
+
+void
+on_ILButtonajoutera_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+gboolean
+on_comboboxrolea_popdown               (GtkComboBox     *combobox,
+                                        gpointer         user_data);
+
+void
+on_ILbuttonmodifiera_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_ILbuttonsupprimera_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_MJbuttonLogin_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_ANbuttonGBV_clicked                 (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_confAjoutManelBV_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RechercheManelBV_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_Modifier1ManelBV_clicked            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_DeleteManelBV_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_Recherche2ManelBV_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_AfficherManelBV_clicked             (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RecherchePManelBV_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_ConfmodifierManelBV_clicked         (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_capobsmanel2_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capobsmanel3_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capobsmanel4_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capobsmanel5_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capobsmanel1_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capobsmanel1_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capobsmanel2_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capobsmanel3_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capobsmanel4_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capobsmanel5_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_cap1ManelModifierBV_toggled         (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_cap3ManelModifierBV_toggled         (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_cap2ManelModifierBV_toggled         (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_radiobutton106_toggled              (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_cap5ManelModifierBV_toggled         (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_treeview16_row_activated            (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+
+void
+on_AccManel_clicked                    (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_radiobutton103_toggled              (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_radiobutton104_toggled              (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_radiobutton105_toggled              (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_radiobutton106_toggled              (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_radiobutton107_toggled              (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_ModifierManel_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_RetourrManell_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_capaciteobs1_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capaciteobs2_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capaciteobs3_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capaciteobs4_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_capaciteobs5_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_treeview10_row_activated            (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+
+void
+on_AccSupprimerManel_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_rechercheMane_clicked               (GtkButton       *button,
+                                        gpointer         user_data);
